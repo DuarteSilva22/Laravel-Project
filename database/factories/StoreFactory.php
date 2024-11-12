7ef83591c383df3488a450a9b45b61817a639896
DuarteSilva22 <duartesilva22@outlook.com>
2024-11-12 15:38:51 +0000

    Add database factories

---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Store;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Store>
+ */
+class StoreFactory extends Factory
+{
+    protected $model = Store::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'mobile_phone' => $this->faker->phoneNumber(),
+            'slug' => $this->faker->slug(),
+            'description' => $this->faker->sentence(),
+
+        ];
+    }
+}
